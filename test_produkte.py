9c9dbfdd08c217078f73ce545782ce51b045b0d4
Liudmyla <liudmylasakovski@gmail.com>
2026-07-11 13:48:23 +0200

    Tests verbessert

--- test_produkte.py
+++ test_produkte.py
@@ -5,7 +5,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 import time
 
-def test_produkte():
+def test_sortierung():
 
     chrome_options = Options()
     chrome_options.add_argument("--incognito")
